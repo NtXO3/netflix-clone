73522f76703fec37baff47092e08e401c96de9d6
Nathan Minkhorst <nathanminkhorst@gmail.com>
2022-05-26 23:30:03 +0200

    cursor no drop

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -23,9 +23,8 @@ const Row = ({ title, fetchUrl, rowId }) => {
         }
     }
 
-    const selectMovie = async (passedId) => {
+    const selectMovie = (passedId) => {
         const selectedMovieEl = document.getElementById('selected-movie' + rowId)
-        const movieElId = document.getElementById(passedId)
         const movieInfo = movies.find(item => item.id === passedId)
         if (selectedMovie.movieInfo === movieInfo) {
             selectedMovieEl.style.maxHeight = 0            
@@ -36,20 +35,9 @@ const Row = ({ title, fetchUrl, rowId }) => {
             setSelectedMovie({...selectedMovie, movieInfo})
             setTimeout(() => {
                 selectedMovieEl.style.maxHeight = 550 + 'px'
-                console.log(selectedMovieEl)
             }, 10)
         }
     }
-
-    // const selectMovie = (passedId) => {
-    //     const selectedMovieEl = document.getElementById('selected-movie')
-    //     const movieInfo = movies.find(item => item.id === passedId)
-    //     if (selectedMovie.movieInfo === movieInfo) {
-    //         setSelectedMovie({}) 
-    //     } else {
-    //         setSelectedMovie({...selectedMovie, movieInfo})
-    //     }
-    // }
 
     return (
         <>
@@ -73,8 +61,8 @@ const Row = ({ title, fetchUrl, rowId }) => {
                         <div className='absolute w-full top-[20%] p-4 md:p-8'>
                             <h1 className='text-3xl md:text-5xl font-bold'>{selectedMovie.movieInfo?.title}</h1>
                             <div className='my-4'>
-                                <button className='border bg-gray-300 text-black border-gray-300 py-2 px-5'>Play</button>
-                                <button className='border text-white border-gray-300 py-2 px-5 ml-4'>Watch Later</button>
+                                <button className='border bg-gray-300 text-black border-gray-300 py-2 px-5 no-cursor'>Play</button>
+                                <button className='border text-white border-gray-300 py-2 px-5 ml-4 no-cursor'>Watch Later</button>
                             </div>
                             <p className='text-gray-400 text-sm'>Released: {selectedMovie.movieInfo?.release_date}</p>
                             <p className='w-full md:max-w-[70%] lg:max-w[50%] xl:max-w-[35%] text-gray-200 my-4' >{
